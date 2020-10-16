8b318441eaca30eb2eb915a89f5ef936ad60c0a6
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-16 21:33:45 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -11,3 +11,17 @@ def find_max_value(scale_2)
   end
   max_value
 end
+
+def find_max_value(scale)
+  scale = [2,3,4,5]
+  count = 0
+  max_value = -1
+
+  while count < scale.length do
+    if max value < scale[count]
+      max_value = scale[count]
+    end
+    count += 1
+  end
+  max_value
+  end

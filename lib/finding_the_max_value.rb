df64a8c0879906275f165c0c1fc6592240781b9c
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-16 21:24:45 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,7 +1,8 @@
 def find_max_value(array)
   array = [1,2,1,3,4,3,5,4,3,2,1]
   count = 0
-  max_value = -1
+  max_value = 0
+  
   while count < array.length do
     if max_value < array[count]
       max_value = array[count]
